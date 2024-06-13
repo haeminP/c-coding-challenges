4fb0cd8ac5432e57c5ff37b00e3e5578ffe0bfeb
Haemin Park <hmin.park1@gmail.com>
2024-06-12 21:59:43 -0400

    get current working directory

---
+++ system_programming/file_directory/ex3.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    char *cwd;
+    char wd[BUFSIZ];
+    
+    cwd = getcwd(NULL, BUFSIZ);
+    printf("1.Current Directory: %s\n", cwd);
+
+    chdir("hanbit");
+
+    getcwd(wd, BUFSIZ);
+    printf("2.Current Directory: %s\n", wd);
+
+    return 0;
+
+}
