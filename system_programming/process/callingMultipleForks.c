9e21f4b779606e2ac3eb11a6463636c88902d8fa
Haemin Park <hmin.park1@gmail.com>
2024-07-06 23:55:16 -0400

    multiple processes example

---
+++ system_programming/process/callingMultipleForks.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main() {
+    int id1 = fork();
+    int id2 = fork();
+    if (id1 == 0) {
+        if (id2 == 0) {
+        printf("We are process y\n");
+        } else {
+        printf("We are process x\n");
+        }
+    } else {
+        if (id2 == 0) {
+            printf("We are process z\n");
+        } else {
+            printf("We are the parent process!\n");
+        }
+    }
+
+    while(wait(NULL) != -1 || errno != ECHILD) {
+        printf("wait for a child to finish");
+    }
+    
+
+
+}
